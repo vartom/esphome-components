3126df2eba361d0bd6471c8cf12d16b5a3a04cb7
Vartom <babanakov_dima@mail.ru>
2025-11-29 15:50:44 +0700

    Update __init__.py
    Fix fan.fan_schema.

--- components/ble_adv_controller/fan/__init__.py
+++ components/ble_adv_controller/fan/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import fan
 
 from esphome.const import (
-    CONF_OUTPUT_ID,
+    CONF_ID,
     CONF_RESTORE_MODE,
 )
 
@@ -15,6 +15,7 @@ from .. import (
 )
 
 from ..const import (
+    CONF_BLE_ADV_CONTROLLER_ID,
     CONF_BLE_ADV_SPEED_COUNT,
     CONF_BLE_ADV_DIRECTION_SUPPORTED,
     CONF_BLE_ADV_OSCILLATION_SUPPORTED,
@@ -24,9 +25,9 @@ from ..const import (
 BleAdvFan = bleadvcontroller_ns.class_('BleAdvFan', fan.Fan, BleAdvEntity)
 
 CONFIG_SCHEMA = cv.All(
-    fan.FAN_SCHEMA.extend(
+    fan.fan_schema(BleAdvFan)
+    .extend(
         {
-            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(BleAdvFan),
             cv.Optional(CONF_BLE_ADV_SPEED_COUNT, default=6): cv.one_of(0,3,6),
             cv.Optional(CONF_BLE_ADV_DIRECTION_SUPPORTED, default=True): cv.boolean,
             cv.Optional(CONF_BLE_ADV_OSCILLATION_SUPPORTED, default=False): cv.boolean,
@@ -34,12 +35,13 @@ CONFIG_SCHEMA = cv.All(
             # override default value for restore mode, to always restore as it was if possible
             cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT_OFF"): cv.enum(fan.RESTORE_MODES, upper=True, space="_"),
         }
-    ).extend(ENTITY_BASE_CONFIG_SCHEMA),
+    )
+    .extend(ENTITY_BASE_CONFIG_SCHEMA),
 )
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await entity_base_code_gen(var, config, "fan")
     await fan.register_fan(var, config)
     cg.add(var.set_speed_count(config[CONF_BLE_ADV_SPEED_COUNT]))
